4338b1bbd31cfbb336c187e83618565994929499
flutterandroid <atblogad1@gmail.com>
2020-02-27 15:34:59 +0700

    Create another version

--- utils/sort.js
+++ utils/sort.js
@@ -2,19 +2,10 @@
 This file contain a class that have functionality to sort array
 */
 
-class SortByLastIndex {
-  constructor(array) {
+class Sort {
+  constructor(array, compare) {
     this.array = array;
-    this.run();
-  }
-
-  compare(x, y) {
-    const xArr = x.trim().split(" ");
-    const yArr = y.trim().split(" ");
-    const xLast = xArr[xArr.length - 1];
-    const yLast = yArr[yArr.length - 1];
-
-    return xLast.localeCompare(yLast, undefined, { sensitivity: "base" });
+    this.compare = compare;
   }
 
   run() {
@@ -23,4 +14,4 @@ class SortByLastIndex {
   }
 }
 
-module.exports = SortByLastIndex;
+module.exports = Sort;
--- utils/sort_by_lastname.js
+++ utils/sort_by_lastname.js
@@ -1,13 +1,22 @@
 const File = require("./file");
-const SortByLastIndex = require("./sort");
+const Sort = require("./sort");
+
+function compare(x, y) {
+  const xArr = x.trim().split(" ");
+  const yArr = y.trim().split(" ");
+  const xLast = xArr[xArr.length - 1];
+  const yLast = yArr[yArr.length - 1];
+
+  return xLast.localeCompare(yLast, undefined, { sensitivity: "base" });
+}
 
 /* This method is supposed to be used with a file that contains string of people names separated by line*/
 function sortByLastName(file) {
   const fileContentArr = File.toArray(file);
 
-  const sortNames = new SortByLastIndex(fileContentArr);
+  const sortNames = new Sort(fileContentArr, compare);
   const sorted = sortNames.run();
-
+  console.log(sorted);
   return sorted;
 }
 
